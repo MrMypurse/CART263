7026822c7f44ce3f596f0f4497331b35de07984e
Janet Sun <s_yiting@cda122ev5635x.local>
2020-01-28 13:05:44 -0500

    A2: add assignment #2

--- Assignments/assignment#2/js/script.js
+++ Assignments/assignment#2/js/script.js
@@ -1,43 +1,87 @@
 "use strict";
 
-/********************************************************************
+/*****************
 
-Title of Project
-Author Name
+Raving Redactionist
+Pippin Barr
 
-This is a template. Fill in the title, author, and this description
-to match your project! Write JavaScript to do amazing things below!
+You are redacting a document, but it keeps becoming unredacted!
+Click the secret information to hide it, don't let all the
+secrets become revealed!
 
-*********************************************************************/
+******************/
+
+// The chance a span will be revealed per update
+const REVEAL_POSSIBILITY = 0.1;
+// How often to update the spans (potentially revealing them)
+const UPDATE_FREQUENCY = 500;
+
+// A place to store the jQuery selection of all spans
 let $spans;
-const interval = 500;
-const probability = 0.2;
+
+// When the document is loaded we call the setup function
 $(document).ready(setup);
 
-
+// setup()
+//
+// Sets the click handler and starts the time loop
 function setup() {
-  // This code will run when the document is ready!
-  $spans = $("span");
-  setInterval(update, interval);
+  // Save the selection of all spans (since we do stuff to them multiple times)
+  $spans = $('span');
+  // Set a click handler on the spans (so we know when they're clicked)
   $spans.on('click', spanClicked);
+  // Set an interval of 500 milliseconds to update the state of the page
+  setInterval(update, UPDATE_FREQUENCY);
+};
 
+// spanClicked()
+//
+// When a span is clicked we remove its revealed class and add the redacted class
+// thus blacking it out
+function spanClicked() {
+  $(this).removeClass('revealed');
+  $(this).addClass('redacted');
 }
 
-function update(){
-  console.log("YAY UPDATE");
+// update()
+//
+// Update is called every 500 milliseconds and it updates all the spans on the page
+// using jQuery's each() function which calls the specified function on _each_ of the
+// elements in the selection
+function update() {
   $spans.each(updateSpan);
 }
 
-function updateSpan(){
-  console.log("YAY SPAN")
-  let number = Math.random();
-  if (number <= probability){
-    $(this).removeClass("redacted");
-    $(this).addClass("revealed");
+// updateSpan()
+//
+// With random chance it unblanks the current span by removing the
+// redacted class and adding the revealed class. Because this function is called
+// by each(), "this" refers to the current element that each has selected.
+function updateSpan() {
+  let r = Math.random();
+  if (r < REVEAL_POSSIBILITY) {
+    $(this).removeClass('redacted');
+    $(this).addClass('revealed');
   }
 }
 
-function spanClicked(){
-  $(this).removeClass("revealed");
-  $(this).addClass("redacted");
-}
+// A version using anonymous functions if you're interested:
+
+// $(document).ready(function () {
+//   $spans = $('span');
+//
+//   $spans.on('click',function () {
+//     $(this).removeClass('revealed');
+//     $(this).addClass('redacted');
+//   });
+//
+//   setInterval(function () {
+//     $spans.each(function () {
+//       let r = Math.random();
+//       if (r < REVEAL_POSSIBILITY) {
+//         $(this).removeClass('redacted');
+//         $(this).addClass('revealed');
+//       }
+//     });
+//   },UPDATE_FREQUENCY);
+// });
