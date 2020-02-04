e5860c20f44d9dfd26c02bab7e6cb7b4180940a1
Janet Sun <s_yiting@cda122ev5635x.local>
2020-02-04 14:44:57 -0500

    P1: add comment
    1. finishing commenting javascript sheet
    2. add more comments in the css sheet

--- Projects/Project01/js/script.js
+++ Projects/Project01/js/script.js
@@ -2,7 +2,7 @@
 
 /********************************************************************
 
-Stirring
+STIRRING
 Janet Sun
 
 This is a mini stirring game. Player's mission is to pick one "perk potion"
@@ -37,21 +37,37 @@ function setup(){
 
 }
 
+//onDrop()
+//
+//dropping the potion in the pot
 function onDrop(event, ui){
+  //check if the potion is dropping
   console.log('DROPPED');
+  //play sound effect of potion pouring
   pourring.play();
+  //stop the potion from being draggable since the player can only pick 1 potion
   $potion.draggable('disable');
   ui.draggable.remove();
+  //call the stir function when the player clicks the button
   $button.click(stir);
 }
 
+//stir()
+//
+//"strring" the pot when the player clickes on the button and by changing the
+//animation frame number every click
 function stir(){
-
+  //Add 1 to the clickNumber everytime the player clicks the button
   clickNumber += 1;
+  //play sound effect of stirring liquid
   stirring.play();
+  //make sure that the clickNumber is adding everytime the player clicks
   console.log(clickNumber);
+  //assign the click number to the frame number to change animation frames
   $pot.attr(`src`,`assets/images/pot${clickNumber}.png`);
+  //make sure that the stir function is working to call the next animation frame
   console.log(stir);
+  //reset the frame number to 0 to loop the animation
   if(clickNumber >= 17){
     clickNumber = 0;
     return;
