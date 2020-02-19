6b4839453517f15bc7164a3ceb45e522c4870c24
Janet Sun <sun19980@gmail.com>
2020-02-18 19:55:19 -0500

    A3: add comments and fix commands
    1. add key to use responsive voice
    2. fix commands code
    3. add certain comments

--- Assignments/assignment03/js/script.js
+++ Assignments/assignment03/js/script.js
@@ -2,181 +2,194 @@
 
 /********************************************************************
 
-Title of Project
-Author Name
+Assignment 03: Slamina Special
+Janet Sun
 
-This is a template. Fill in the title, author, and this description
-to match your project! Write JavaScript to do amazing things below!
+Speak to the game! If you give the game some love, the game will speak to you.
 
 *********************************************************************/
-if (annyang){
+
+
+
+//Array of cuteness
+let animals = [
+  "Aardvark",
+  "Alligator",
+  "Alpaca",
+  "Antelope",
+  "Ape",
+  "Armadillo",
+  "Baboon",
+  "Badger",
+  "Bat",
+  "Bear",
+  "Beaver",
+  "Bison",
+  "Boar",
+  "Buffalo",
+  "Bull",
+  "Camel",
+  "Canary",
+  "Capybara",
+  "Cat",
+  "Chameleon",
+  "Cheetah",
+  "Chimpanzee",
+  "Chinchilla",
+  "Chipmunk",
+  "Cougar",
+  "Cow",
+  "Coyote",
+  "Crocodile",
+  "Crow",
+  "Deer",
+  "Dingo",
+  "Dog",
+  "Donkey",
+  "Dromedary",
+  "Elephant",
+  "Elk",
+  "Ewe",
+  "Ferret",
+  "Finch",
+  "Fish",
+  "Fox",
+  "Frog",
+  "Gazelle",
+  "Gila monster",
+  "Giraffe",
+  "Gnu",
+  "Goat",
+  "Gopher",
+  "Gorilla",
+  "Grizzly bear",
+  "Ground hog",
+  "Guinea pig",
+  "Hamster",
+  "Hedgehog",
+  "Hippopotamus",
+  "Hog",
+  "Horse",
+  "Hyena",
+  "Ibex",
+  "Iguana",
+  "Impala",
+  "Jackal",
+  "Jaguar",
+  "Kangaroo",
+  "Koala",
+  "Lamb",
+  "Lemur",
+  "Leopard",
+  "Lion",
+  "Lizard",
+  "Llama",
+  "Lynx",
+  "Mandrill",
+  "Marmoset",
+  "Mink",
+  "Mole",
+  "Mongoose",
+  "Monkey",
+  "Moose",
+  "Mountain goat",
+  "Mouse",
+  "Mule",
+  "Muskrat",
+  "Mustang",
+  "Mynah bird",
+  "Newt",
+  "Ocelot",
+  "Opossum",
+  "Orangutan",
+  "Oryx",
+  "Otter",
+  "Ox",
+  "Panda",
+  "Panther",
+  "Parakeet",
+  "Parrot",
+  "Pig",
+  "Platypus",
+  "Polar bear",
+  "Porcupine",
+  "Porpoise",
+  "Prairie dog",
+  "Puma",
+  "Rabbit",
+  "Raccoon",
+  "Ram",
+  "Rat",
+  "Reindeer",
+  "Reptile",
+  "Rhinoceros",
+  "Salamander",
+  "Seal",
+  "Sheep",
+  "Shrew",
+  "Silver fox",
+  "Skunk",
+  "Sloth",
+  "Snake",
+  "Squirrel",
+  "Tapir",
+  "Tiger",
+  "Toad",
+  "Turtle",
+  "Walrus",
+  "Warthog",
+  "Weasel",
+  "Whale",
+  "Wildcat",
+  "Wolf",
+  "Wolverine",
+  "Wombat",
+  "Woodchuck",
+  "Yak",
+  "Zebra"
+];
+
+//Array of harassments
+let insults = [
+  'You are terrible',
+  'stupid',
+  'try harder',
+  'You are dumb',
+]
+
+let correctAnimal;
+let answers = [];
+let answer;
+let score = 0;
+const NUM_OPTIONS = 7;
+
+if (annyang) {
   var commands = {
-    'hello': function(){alert('Hello Mortals.');}
-  };
-  var commands1 = {
-    'I am a disappointment': function(){
-    gaveUp();}
-  };
-  var commands2 = {
-    'Say it again': function(){
-    sayBackwards(correctAnimal);}
+    'hello': function() {
+      alert('Hello, Mortals.');
+    },
+    'I am a disappointment': function() {
+      gaveUp();
+    },
+    'Say it again': function() {
+      sayBackwards(correctAnimal);
+    },
+    'I think it is *answer': function(){
+      checkAnswer();
+    },
   };
 
   annyang.addCommands(commands);
-  annyang.addCommands(commands1);
-  annyang.addCommands(commands2);
   annyang.start();
 }
 
 $(document).ready(setup);
-let animals = [
-      "Aardvark",
-      "Alligator",
-      "Alpaca",
-      "Antelope",
-      "Ape",
-      "Armadillo",
-      "Baboon",
-      "Badger",
-      "Bat",
-      "Bear",
-      "Beaver",
-      "Bison",
-      "Boar",
-      "Buffalo",
-      "Bull",
-      "Camel",
-      "Canary",
-      "Capybara",
-      "Cat",
-      "Chameleon",
-      "Cheetah",
-      "Chimpanzee",
-      "Chinchilla",
-      "Chipmunk",
-      "Cougar",
-      "Cow",
-      "Coyote",
-      "Crocodile",
-      "Crow",
-      "Deer",
-      "Dingo",
-      "Dog",
-      "Donkey",
-      "Dromedary",
-      "Elephant",
-      "Elk",
-      "Ewe",
-      "Ferret",
-      "Finch",
-      "Fish",
-      "Fox",
-      "Frog",
-      "Gazelle",
-      "Gila monster",
-      "Giraffe",
-      "Gnu",
-      "Goat",
-      "Gopher",
-      "Gorilla",
-      "Grizzly bear",
-      "Ground hog",
-      "Guinea pig",
-      "Hamster",
-      "Hedgehog",
-      "Hippopotamus",
-      "Hog",
-      "Horse",
-      "Hyena",
-      "Ibex",
-      "Iguana",
-      "Impala",
-      "Jackal",
-      "Jaguar",
-      "Kangaroo",
-      "Koala",
-      "Lamb",
-      "Lemur",
-      "Leopard",
-      "Lion",
-      "Lizard",
-      "Llama",
-      "Lynx",
-      "Mandrill",
-      "Marmoset",
-      "Mink",
-      "Mole",
-      "Mongoose",
-      "Monkey",
-      "Moose",
-      "Mountain goat",
-      "Mouse",
-      "Mule",
-      "Muskrat",
-      "Mustang",
-      "Mynah bird",
-      "Newt",
-      "Ocelot",
-      "Opossum",
-      "Orangutan",
-      "Oryx",
-      "Otter",
-      "Ox",
-      "Panda",
-      "Panther",
-      "Parakeet",
-      "Parrot",
-      "Pig",
-      "Platypus",
-      "Polar bear",
-      "Porcupine",
-      "Porpoise",
-      "Prairie dog",
-      "Puma",
-      "Rabbit",
-      "Raccoon",
-      "Ram",
-      "Rat",
-      "Reindeer",
-      "Reptile",
-      "Rhinoceros",
-      "Salamander",
-      "Seal",
-      "Sheep",
-      "Shrew",
-      "Silver fox",
-      "Skunk",
-      "Sloth",
-      "Snake",
-      "Squirrel",
-      "Tapir",
-      "Tiger",
-      "Toad",
-      "Turtle",
-      "Walrus",
-      "Warthog",
-      "Weasel",
-      "Whale",
-      "Wildcat",
-      "Wolf",
-      "Wolverine",
-      "Wombat",
-      "Woodchuck",
-      "Yak",
-      "Zebra"
-    ];
-
-let correctAnimal;
-let answers = [];
-let score = 0;
-const NUM_OPTIONS = 7;
-
 
 function setup() {
   newRound();
 }
 
-function addButton(label){
+function addButton(label) {
   let $createButton = $('<div></div>');
   $createButton.addClass('guess');
   $createButton.text(label);
@@ -185,48 +198,53 @@ function addButton(label){
   $createButton.on('click', handleGuess);
 }
 
-function newRound(){
+function newRound() {
   $('.guess').remove();
   answers = [];
   for (var i = 0; i < NUM_OPTIONS; i++) {
-    let chosenAnimals = animals[Math.floor(Math.random()* animals.length)];
+    let chosenAnimals = animals[Math.floor(Math.random() * animals.length)];
     addButton(chosenAnimals);
     answers.push(chosenAnimals);
   }
-  correctAnimal = answers[Math.floor(Math.random()* answers.length)];
+  correctAnimal = answers[Math.floor(Math.random() * answers.length)];
   sayBackwards(correctAnimal);
   updateScore();
 
 }
 
-function handleGuess(){
+function handleGuess() {
   if ($(this).text() === correctAnimal) {
     setTimeout(newRound, 500);
-    score ++;
+    score++;
     updateScore();
-  }else{
+  } else {
     $('.guess').effect('shake');
     sayBackwards(correctAnimal);
     score = 0;
     updateScore();
   }
-  giveUp();
 }
 
-function sayBackwards(text){
+function sayBackwards(text) {
   let backwardsText = text.split('').reverse().join('');
   let options = {
-    pitch: Math.random(0,2),
-    rate: Math.random(0,2)
+    pitch: Math.random(0, 2),
+    rate: Math.random(0, 2)
   }
-    responsiveVoice.speak(backwardsText, 'Japanese Female',{options});
+  responsiveVoice.speak(backwardsText, 'Japanese Female', {
+    options
+  });
 }
 
-function updateScore(){
+function updateScore() {
   $('#scoreNumber').text(score);
 }
 
-function gaveUp(){
+function giveUp() {
   $('correctAnimal').toggle('highlight');
   setTimeout(newRound(), 2000);
+}
+
+function checkAnswer(){
+
 }
