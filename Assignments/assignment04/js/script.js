556e24324be1f7e5e747562a1d9b6df264fcee27
Janet Sun <sun19980@gmail.com>
2020-03-12 18:49:29 -0400

    A4: add sound
    1. add sound file
    2. add clicksound function

--- Assignments/assignment04/js/script.js
+++ Assignments/assignment04/js/script.js
@@ -11,15 +11,17 @@ to match your project! Write JavaScript to do amazing things below!
 *********************************************************************/
 
 $(document).ready(setup);
-
+//sound effect
+const click = new Audio('assets/sounds/click.wav');
 
 function setup() {
   $.getJSON('data/data.json')
     .done(dataLoaded)
     .fail(dataNotLoaded);
+  clickSound();
 }
 
-function dataLoaded(data){
+function dataLoaded(data) {
   console.log(data);
 
   let randomMenu = getRandomArrayElement(data.menuItems);
@@ -44,41 +46,50 @@ function dataLoaded(data){
   console.log(vowelWord1);
   console.log(vowelWord2);
 
-  if (vowelWord1 === 'A'||
-      vowelWord1 === 'E'||
-      vowelWord1 === 'I'||
-      vowelWord1 === 'O'||
-      vowelWord1 === 'U') {
-     determiner1 = 'an';
-   }else if (randomMenu.charAt(randomMenu.length - 1) === 's'){
-     determiner1 = '';
-   }
+  if (vowelWord1 === 'A' ||
+    vowelWord1 === 'E' ||
+    vowelWord1 === 'I' ||
+    vowelWord1 === 'O' ||
+    vowelWord1 === 'U') {
+    determiner1 = 'an';
+  } else if (randomMenu.charAt(randomMenu.length - 1) === 's') {
+    determiner1 = '';
+  }
 
-   if (vowelWord2 === 'a'||
-       vowelWord2 === 'e'||
-       vowelWord2 === 'i'||
-       vowelWord2 === 'o'||
-       vowelWord2 === 'u') {
-      determiner2 = 'an';
-    }
+  if (vowelWord2 === 'a' ||
+    vowelWord2 === 'e' ||
+    vowelWord2 === 'i' ||
+    vowelWord2 === 'o' ||
+    vowelWord2 === 'u') {
+    determiner2 = 'an';
+  }
 
   let randomDescription = `Eating ${determiner1} ${randomMenu} with ${randomCondiment}
                           is like listening to ${randomGenre} in ${determiner2}
                           ${randomRoom}. It makes me feel ${randomMood}.`;
 
-  $('h1').append(randomDescription);
+  $('.box1').append(randomDescription);
 }
 
 
-function dataNotLoaded(request, text, error){
+function dataNotLoaded(request, text, error) {
   console.error('you fucked up');
 }
 
-function getRandomArrayElement(array){
+function getRandomArrayElement(array) {
   let element = array[Math.floor(Math.random() * array.length)];
   return element;
 }
 
-function clickToRefresh(){
+function clickToRefresh() {
 
+}
+
+//clickSound()
+//
+//a function to make cute clicking sound
+function clickSound() {
+  $('body').click(function() {
+    click.play();
+  })
 }
