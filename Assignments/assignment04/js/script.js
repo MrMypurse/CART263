22c53e5a18f0699ad462b68d50f7911bafa00cc6
Janet Sun <sun19980@gmail.com>
2020-03-12 20:16:27 -0400

    A4: add more comments & beautify

--- Assignments/assignment04/js/script.js
+++ Assignments/assignment04/js/script.js
@@ -2,76 +2,80 @@
 
 /********************************************************************
 
-Title of Project
-Author Name
+ANOTHER ONE
+Janet Sun
 
-This is a template. Fill in the title, author, and this description
-to match your project! Write JavaScript to do amazing things below!
+This webpage will never run out of random sentences as long as you don't
+get tired of it!
 
 *********************************************************************/
-
+//load everything
 $(document).ready(setup);
+//set up variables
 let randomDescription;
 let createSentence;
-//sound effect
+//set up sound effect
 const click = new Audio('assets/sounds/click.wav');
 
+//setup
+//
+//this is a function to setup Annyang and to call other functions
 function setup() {
-
+  //get the data base and display message if not loaded correctly
   $.getJSON('data/data.json')
     .done(dataLoaded)
     .fail(dataNotLoaded);
+  //call other function to display sound and to refresh the sentence using a
+  //button
   clickSound();
   clickToRefresh();
-
+  //set up Annyang
   if (annyang) {
-    // Let's define our first command. First the text we expect, and then the function it should call
+    //Call the function after detecting the spoken sentence
     var commands = {
       'another one': speakToRefresh
     };
-    // Add our commands to annyang
     annyang.addCommands(commands);
-
-    // Start listening. You can call this here, or attach this call to an event, button, etc.
     annyang.start();
   }
 }
 
+//dataLoaded(data)
+//
+//a function to pick random items and display them as a sentence in a class
+//only works if the data is loaded correctly
 function dataLoaded(data) {
   console.log(data);
-
   let randomMenu = getRandomArrayElement(data.menuItems);
   console.log(randomMenu);
-
   let randomCondiment = getRandomArrayElement(data.condiments);
   console.log(randomCondiment);
-
   let randomGenre = getRandomArrayElement(data.genres);
   console.log(randomGenre);
-
   let randomRoom = getRandomArrayElement(data.rooms);
   console.log(randomRoom);
-
   let randomMood = getRandomArrayElement(data.moods);
   console.log(randomMood);
-
+  //set up determiners as variables
   let determiner1 = 'a';
   let determiner2 = 'a';
+  //detect the first letter of the word to determine determiners
   let vowelWord1 = randomMenu.charAt(0);
   let vowelWord2 = randomRoom.charAt(0);
   console.log(vowelWord1);
   console.log(vowelWord2);
-
+  //if the vowel is the first letter, use 'an' as a determiner
   if (vowelWord1 === 'A' ||
     vowelWord1 === 'E' ||
     vowelWord1 === 'I' ||
     vowelWord1 === 'O' ||
     vowelWord1 === 'U') {
     determiner1 = 'an';
+    //if the object is plural, no determiner
   } else if (randomMenu.charAt(randomMenu.length - 1) === 's') {
     determiner1 = '';
   }
-
+  //if the vowel is the first letter, use 'an' as a determiner
   if (vowelWord2 === 'a' ||
     vowelWord2 === 'e' ||
     vowelWord2 === 'i' ||
@@ -79,27 +83,38 @@ function dataLoaded(data) {
     vowelWord2 === 'u') {
     determiner2 = 'an';
   }
-
+  //Construct the sentence using the randomly selected words
   randomDescription = `Eating ${determiner1} ${randomMenu} with ${randomCondiment}
                           is like listening to ${randomGenre} in ${determiner2}
                           ${randomRoom}. It makes me feel ${randomMood}.`;
+  //Make a div box for the sentence and store the sentence in
   createSentence = $('<div></div>');
   createSentence.addClass('sentence');
   createSentence.text(randomDescription);
   createSentence.appendTo('.box1');
 }
 
+//dataNotLoaded()
+//
+//a function to insult you if the data isn't correctly loaded
 function dataNotLoaded(request, text, error) {
-  console.error('you fucked up');
+  console.error('You fucked up.');
 }
 
+//getRandomArrayElement(array)
+//
+//a function to pick random element from an array
 function getRandomArrayElement(array) {
   let element = array[Math.floor(Math.random() * array.length)];
   return element;
 }
 
+//clickToRefresh()
+//
+//a function to erase the old sentence and make a new sentence when the button
+//is clicked
 function clickToRefresh() {
-  $('#refreshButton').click(function(){
+  $('#refreshButton').click(function() {
     $('.sentence').remove();
     $.getJSON('data/data.json')
       .done(dataLoaded)
@@ -107,7 +122,11 @@ function clickToRefresh() {
   })
 }
 
-function speakToRefresh(){
+//speakToRefresh()
+//
+//a function to erase the old sentence and make a new sentence when the voice
+//commande is detected
+function speakToRefresh() {
   $('.sentence').remove();
   $.getJSON('data/data.json')
     .done(dataLoaded)
