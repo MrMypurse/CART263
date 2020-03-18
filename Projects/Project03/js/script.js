915ea9350e2fba0e2089adc7206166078de3ec7a
Janet Sun <sun19980@gmail.com>
2020-03-18 10:18:57 -0400

    P3: create triangle shape

--- Projects/Project03/js/script.js
+++ Projects/Project03/js/script.js
@@ -9,11 +9,15 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+let cols;
+let rows;
+let scale = 20;
+let w = 600;
+let h = 600;
 
 // preload()
 //
 // Description of preload
-
 function preload() {
 
 }
@@ -24,7 +28,12 @@ function preload() {
 // Description of setup
 
 function setup() {
-
+  createCanvas(600, 600, WEBGL);
+  background(0);
+  stroke(255);
+  noFill();
+  cols = w/scale;
+  rows = h/scale;
 }
 
 
@@ -33,5 +42,12 @@ function setup() {
 // Description of draw()
 
 function draw() {
-
-}
+  for(let y = 0; y < rows; y++){
+    beginShape(TRIANGLE_STRIP);
+  for (let x = 0; x < cols; x++){
+    vertex(x * scale, y * scale);
+    vertex(x * scale, (y + 1)* scale);
+    }
+    endShape();
+    }
+  }
