35fd3afd25f3d4abb88cc199733d535939dc7408
Janet Sun <sun19980@gmail.com>
2020-03-18 11:47:09 -0400

    P3: try to set up z value

--- Projects/Project03/js/script.js
+++ Projects/Project03/js/script.js
@@ -15,34 +15,35 @@ let w = 1000;
 let h = 1000;
 let cols = w/scale;
 let rows = h/scale;
+let terrainZ = [];
 let noiseScale;
-
 
 // preload()
 //
 // Description of preload
 function preload() {
-
 }
-
 
 // setup()
 //
 // Description of setup
-
 function setup() {
   createCanvas(1000, 800, WEBGL);
   resizeCanvas(width, height);
   background(0);
   stroke(255);
   noFill();
+  terrainZ = [cols][rows];
+  for(let y = 0; y < rows; y++){
+  for (let x = 0; x < cols; x++){
+    terrainZ[x][y] = random(10,-10);
+  }
 }
-
+}
 
 // draw()
 //
 // Description of draw()
-
 function draw() {
   rotateX(PI/3);
   translate(-w/2, -h/4);
