87c2a768320c018bf57654610acda600773b2663
Janet Sun <sun19980@gmail.com>
2020-03-24 16:08:47 -0400

    P3: fix collision and animation problem

--- Projects/Project03/js/script.js
+++ Projects/Project03/js/script.js
@@ -11,7 +11,7 @@ to match your project! Write JavaScript to do amazing things below!
 *********************************************************************/
 
 $(document).ready(setup);
-
+let wateringInterval;
 let $tree;
 let $watercan;
 let waterlevel = 0;;
@@ -24,35 +24,26 @@ function setup() {
     drop: onDrop
   });
   $watercan.on("mousedown", function() {
-
   })
   $watercan.on("mouseup", function() {
-
+    wateringCollision();
   })
 }
 
 function onDrop(event, ui) {
   console.log("DROPPED");
-  //ui.draggable.remove();
-  setInterval(checkCollision, 300);
+//  ui.draggable.remove();
+  //setInterval(checkCollision, 300);
 }
 
-function watering() {
-  if ($watercan.attr("src") === "assets/images/watercan0.png") {
-    $watercan.attr("src", "assets/images/watercan1.png");
-    waterlevel = waterlevel + 5;
-  } else {
-    $watercan.attr("src", "assets/images/watercan0.png");
-  }
-}
 
-function getPositions(tree) {
-  let treePosition = $tree.position();
-  let treeWidth = $tree.width();
-  let treeHeight = $tree.height();
+function getPositions(object) {
+  let objectPosition = object.position();
+  let objectWidth = object.width();
+  let objectHeight = object.height();
   return [
-    [treePosition.left, treePosition.left + treeWidth],
-    [treePosition.top, treePosition.top + treeHeight]
+    [objectPosition.left, objectPosition.left + objectWidth],
+    [objectPosition.top, objectPosition.top + objectHeight]
   ];
 }
 
@@ -62,18 +53,36 @@ function comparePositions(p1, p2) {
   return x1[1] > x2[0] || x1[0] === x2[0] ? true : false;
 }
 
-function checkCollision() {
-  let treeBox = $('#tree')[0];
-  let position1 = getPositions(treeBox);
-  let position2 = getPositions(this);
+function checkCollision(a, b) {
+  //let treeBox = $('#tree')[0];
+
+  let position1 = getPositions(a);
+  let position2 = getPositions(b);
   let horizontalMatch = comparePositions(position1[0], position2[0]);
   let verticalMatch = comparePositions(position1[1], position2[1]);
   let match = horizontalMatch && verticalMatch;
-  if (match) {
-    $('body').append('<p> HITTT </p>');
-    watering();
+  return match;
+}
+
+function wateringAnimation() {
+  if ($watercan.attr("src") === "assets/images/watercan0.png") {
+    $watercan.attr("src", "assets/images/watercan1.png");
+    waterlevel = waterlevel + 5;
   } else {
     $watercan.attr("src", "assets/images/watercan0.png");
   }
+}
 
+function wateringCollision(){
+  let collision = checkCollision($watercan, $tree);
+  if (collision === true) {
+    $('body').append('<p> HITTT </p>');
+    if (!wateringInterval) {
+        wateringInterval = setInterval(wateringAnimation, 300);
+    }
+  } else {
+    clearInterval(wateringInterval);
+    wateringInterval = false;
+    $watercan.attr("src", "assets/images/watercan0.png");
+  }
 }
