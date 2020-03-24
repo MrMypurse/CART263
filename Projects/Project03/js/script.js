40d022a0bc2da7f4dcf26bf0455dc3d1135b51fd
Janet Sun <sun19980@gmail.com>
2020-03-24 15:26:21 -0400

    P3: work on the collision
    1. collision is working
    2. will work on how to stop animation when images are not in collision

--- Projects/Project03/js/script.js
+++ Projects/Project03/js/script.js
@@ -14,34 +14,66 @@ $(document).ready(setup);
 
 let $tree;
 let $watercan;
+let waterlevel = 0;;
 
-function setup(){
+function setup() {
   $tree = $('#tree');
   $watercan = $('#watercan');
   $watercan.draggable();
   $tree.droppable({
     drop: onDrop
   });
-  $watercan.on("mousedown", function(){
-    //buzz.play();
+  $watercan.on("mousedown", function() {
+
   })
-  $watercan.on("mouseup", function(){
-    //buzz.pause();
+  $watercan.on("mouseup", function() {
+
   })
 }
 
-function onDrop(event, ui){
+function onDrop(event, ui) {
   console.log("DROPPED");
   //ui.draggable.remove();
-//  buzz.pause();
-  setInterval(watering, 300);
+  setInterval(checkCollision, 300);
 }
 
-function watering(){
-  if($watercan.attr("src") === "assets/images/watercan0.png"){
+function watering() {
+  if ($watercan.attr("src") === "assets/images/watercan0.png") {
     $watercan.attr("src", "assets/images/watercan1.png");
-    //crunch.play();
-  }else{
+    waterlevel = waterlevel + 5;
+  } else {
     $watercan.attr("src", "assets/images/watercan0.png");
   }
+}
+
+function getPositions(tree) {
+  let treePosition = $tree.position();
+  let treeWidth = $tree.width();
+  let treeHeight = $tree.height();
+  return [
+    [treePosition.left, treePosition.left + treeWidth],
+    [treePosition.top, treePosition.top + treeHeight]
+  ];
+}
+
+function comparePositions(p1, p2) {
+  let x1 = p1[0] < p2[0] ? p1 : p2;
+  let x2 = p1[0] < p2[0] ? p2 : p1;
+  return x1[1] > x2[0] || x1[0] === x2[0] ? true : false;
+}
+
+function checkCollision() {
+  let treeBox = $('#tree')[0];
+  let position1 = getPositions(treeBox);
+  let position2 = getPositions(this);
+  let horizontalMatch = comparePositions(position1[0], position2[0]);
+  let verticalMatch = comparePositions(position1[1], position2[1]);
+  let match = horizontalMatch && verticalMatch;
+  if (match) {
+    $('body').append('<p> HITTT </p>');
+    watering();
+  } else {
+    $watercan.attr("src", "assets/images/watercan0.png");
+  }
+
 }
