25774f5ff0a3fb2c935f2067e3f7672369e1b396
Janet Sun <sun19980@gmail.com>
2020-03-24 14:24:54 -0400

    P3: set up basic drag & drop

--- Projects/Project03/js/script.js
+++ Projects/Project03/js/script.js
@@ -12,7 +12,36 @@ to match your project! Write JavaScript to do amazing things below!
 
 $(document).ready(setup);
 
+let $tree;
+let $watercan;
 
-function setup() {
+function setup(){
+  $tree = $('#tree');
+  $watercan = $('#watercan');
+  $watercan.draggable();
+  $tree.droppable({
+    drop: onDrop
+  });
+  $watercan.on("mousedown", function(){
+    //buzz.play();
+  })
+  $watercan.on("mouseup", function(){
+    //buzz.pause();
+  })
+}
 
+function onDrop(event, ui){
+  console.log("DROPPED");
+  //ui.draggable.remove();
+//  buzz.pause();
+  setInterval(watering, 300);
+}
+
+function watering(){
+  if($watercan.attr("src") === "assets/images/watercan0.png"){
+    $watercan.attr("src", "assets/images/watercan1.png");
+    //crunch.play();
+  }else{
+    $watercan.attr("src", "assets/images/watercan0.png");
+  }
 }
